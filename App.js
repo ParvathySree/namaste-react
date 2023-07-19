780bfb2a0b361b580ef24bd6f9f1392481aa5eeb
ParvathySree <sreeparvathylaiju@gmail.com>
2023-07-19 16:28:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+// const heading = React.createElement("h1",{id:"heading"},"Hello from react")
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading1"},"Children"),React.createElement("h2",{id:"heading2"},"Hello from react")]))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
