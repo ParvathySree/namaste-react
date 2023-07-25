7df8f3a5316a3d369d795bd8183d16f5eea69372
ParvathySree <sreeparvathylaiju@gmail.com>
2023-07-25 17:51:49 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-// const heading = React.createElement("h1",{id:"heading"},"Hello from react")
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading)
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading1"},"Children"),React.createElement("h2",{id:"heading2"},"Hello from react")]))
+
+const Heading = () => {
+    return <h1>Hello World</h1>
+};
+
+const Container = () =>{
+    return <div><h2>Hi</h2><Heading/>{Heading()}</div>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+root.render(<Container/>)
